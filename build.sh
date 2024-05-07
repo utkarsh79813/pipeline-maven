1f6e685612b52f22d96337cfda798e0342ad4905
utkarsh79813 <162578396+utkarsh79813@users.noreply.github.com>
2024-05-07 17:40:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
 echo "you have pass the parameter as: $@"
 #name= $@
 docker run --rm -it -v "$(WORKSPACE)/java_app":/app -v $(WORKSPACE)/.m2:/root/.m2 -w /app maven:latest $@
-#mvn -B -DskipTests clean package
-sleep 5
-cp $(WORKSPACE)/java_app/target/my-app-1.0-SNAPSHOT.jar $(WORKSPACE)/jenkins/build/
+#mvn -B -DskipTests clean package###
+##sleep 5
+##cp $(WORKSPACE)/java_app/target/my-app-1.0-SNAPSHOT.jar $(WORKSPACE)/jenkins/build/
 
 echo "***********************"
 echo "Building Docker Images"
-sleep 5
-export BUILD_TAG=10
-cd jenkins/build && docker-compose -f docker-compose-build.yml build --no-cache
+##sleep 5
+##export BUILD_TAG=10
+####cd jenkins/build && docker-compose -f docker-compose-build.yml build --no-cache
 #sleep 3
 #sh docker images|grep -i app
